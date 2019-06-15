e0d5bef848ec230792a9c32f90d5e3ed0df3bd37
collins kabali <51477747+ckabali@users.noreply.github.com>
2019-06-15 15:11:32 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+
 
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
@@ -12,41 +7,6 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to BongoHive Consult </Text>
-        <Text style={styles.instructions}>
-          Check the readme for the required task
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
-
-import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
-
-export default class FlatListBasics extends Component {
-  render() {
-    return (
-       <View style={styles.container}>
         <FlatList
           data={[
             {key: 'Devin'},
@@ -67,13 +27,9 @@ export default class FlatListBasics extends Component {
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-})
-AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
+ 
